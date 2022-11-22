6f1d2ec2c5b1e78c09e22c042a63ab70f85ddd38
Silvina <silvinagattas@gmail.com>
2022-11-21 23:44:51 -0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -230,7 +230,7 @@ window.addEventListener('beforeinstallprompt', function (e) {
 function showAddToHomeScreen() {
   var a2hsBtn = document.querySelector(".ad2hs-prompt");
   a2hsBtn.style.display = "block";
-  a2hsBtn.addEventListener("click", addToHomeScreen);
+  a2hsBtn.addEventListener("click", showAddToHomeScreen());
 
 }
 
